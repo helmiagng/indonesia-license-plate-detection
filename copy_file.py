a2ded8d15a614c15a31ef8014cb5a08dd76e9915
Helmi Agung <68595414+helmiagng@users.noreply.github.com>
2022-09-27 09:41:36 +0700

    Update copy_file.py

--- copy_file.py
+++ copy_file.py
@@ -6,23 +6,23 @@ def copyfile_to_dest_folder(fpath,dpath,dir):
         src=os.path.join(fpath,file)
         dest=os.path.join(dpath,file)
         try:
-            #copying file to destination folder
-            shutil.copyfile(src,dest)
-            print(f"SUCCESS:{src} copied to {dest}")
+            if not os.path.exists(dest):
+                shutil.copyfile(src,dest)
+                print(f"SUCCES:{src} copied to {dest}")
+            else:
+                print(f"{dest} is exist")
         except Exception as e:
             print(f"ERROR:{e}")
 
 def main():
-    #enter source folder path
     folder_path=str(input("Enter source folder path:"))
-    #enter destination folder path
     dest_folder=str(input("Enter destination folder path:"))
     folder_dir=os.listdir(folder_path)
     copyfile_to_dest_folder(folder_path,dest_folder,folder_dir)
-    
+
     print(f"destination size directory before coying file: {len(os.listdir(dest_folder))}")
     print(f"destination size directory after coying file: {len(os.listdir(dest_folder))}")
-    
+
 if __name__=='__main__':
     print("COPYING FILE IN FOLDER DIRECTORY")
     main()
