1c6c9313e6a708e2cad1e62ccec33ea53a9b85cf
Helmi Agung <68595414+helmiagng@users.noreply.github.com>
2022-09-29 02:25:03 +0700

    Update copy_file.py

--- copy_file.py
+++ copy_file.py
@@ -1,27 +1,21 @@
 import shutil
 import os
-
-def copyfile_to_dest_folder(fpath,dpath,dir):
-    for file in dir:
-        src=os.path.join(fpath,file)
-        dest=os.path.join(dpath,file)
-        try:
-            if not os.path.exists(dest):
-                shutil.copyfile(src,dest)
-                print(f"SUCCES:{src} copied to {dest}")
-            else:
-                print(f"{dest} is exist")
-        except Exception as e:
-            print(f"ERROR:{e}")
-
 def main():
     folder_path=str(input("Enter source folder path:"))
     dest_folder=str(input("Enter destination folder path:"))
     folder_dir=os.listdir(folder_path)
-    copyfile_to_dest_folder(folder_path,dest_folder,folder_dir)
-
-    print(f"destination size directory before coying file: {len(os.listdir(dest_folder))}")
-    print(f"destination size directory after coying file: {len(os.listdir(dest_folder))}")
+    old_size_dir=len(dest_folder)
+    for file in folder_dir:
+        src=os.path.join(folder_path,file)
+        dest=os.path.join(dest_folder,file)
+        try:
+            shutil.copyfile(src,dest)
+            print(f"SUCCES:{src} copied to {dest}")
+        except Exception as e:
+            print(f"ERROR:{e}")
+    
+    print(f"SIZE BEFORE COPYING OF DESTINATION DIRECTORY  :{old_size_dir}")
+    print(f"SIZE AFTER COPYING FILE OF DESTINATION DIRECTORY:{len(os.listdir(dest_folder))}")
 
 if __name__=='__main__':
     print("COPYING FILE IN FOLDER DIRECTORY")
